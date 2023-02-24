39cc2886263d068dd7d1231e15f667cd8012fca5
YersiniaPestis1989 <azentsova@list.ru>
2023-02-24 12:50:26 +0700

    1

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,21 +1,28 @@
 "use sctrict"
-const purpose = 400000;
-let money =  Number(prompt("Ваш месячный доход", 1000));
-let expenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", "еда, квартплата");
-let amount =  Number(prompt("Во сколько обойдутся обязательные статьи расходов?", 1000));
+const purpose = 400000; //цель
+let profit = "10000"; //дополнительный доход
+let money =  Number(prompt("Ваш месячный доход", 1000)); // месячный доход
+let extraMoney = Number(prompt(`Перечислите возможный доход за ваши дополнительные работы: ${profit}?`)); // возможный доход
+let expenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую", "еда, квартплата"); // возможные расходы
+let amount =  Number(prompt("Во сколько обойдутся обязательные статьи расходов?", 1000)); // обязательные расходы
 let deposit =  confirm("Есть ли у вас вклад в банке?")
 
-while (isNaN(money)  || isNaN(amount)) {
+while (isNaN(money)  || isNaN(amount) || isNaN(extraMoney)) {
     alert("Введите данные заново, обращая внимание на формат");
     let money =  Number(prompt("Ваш месячный доход", 1000));
     let amount =  Number(prompt("Во сколько обойдутся обязательные статьи расходов?", 1000));
+    let extraMoney = promt(`Перечислите возможный доход за ваши дополнительные работы: ${profit}?`);
 }
 
-let budgetMonth = money - amount;
-let budgetDay = Math.floor((money - amount) / 30);
-console.log (`Месячный бюджет — ${budgetMonth} рублей`);
-console.log (`Накопить ${purpose} рублей получится через ${Math.ceil(purpose/budgetMonth)} месяцев`);
-console.log (`Дневной бюджет — ${budgetDay} рублей`);
+const getAccumulatedIncome = (x, y, z) => {return (x + y) - z};
+const getTargetMonth = (sum, purpose) => {Math.ceil(purpose/sum)};
+
+let accumulatedIncome = getAccumulatedIncome(money, extraMoney, amount); // прибыль
+let budgetDay = Math.floor(accumulatedIncome/30); // бюджет на день
+
+console.log(`Ваш бюджет на месяц с учетом ваших расходов составляет:  ${getAccumulatedIncome(money, extraMoney, amount)} рублей`);
+console.log(`Ваша цель накопить ${purpose} рублей с учетом всех ваших расходов будет достигнута через`, getTargetMonth(accumulatedIncome, purpose) + ' месяца');
+console.log(`Дневной бюджет: ${budgetDay} рублей`);
 
 if (budgetDay > 6000)
 {
