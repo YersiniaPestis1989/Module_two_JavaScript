7fa3be3546e2f2ea2ccc33cf24da3c27d24d8e4e
YersiniaPestis1989 <azentsova@list.ru>
2023-03-04 22:20:31 +0700

    final

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,13 +1,15 @@
 "use sctrict"
 
 let number = Math.floor(Math.random() * 10) + 1;
+let count = 3;
 function bot() {
         userNumber = Number(prompt("Введите любое число от 1 до 10"));
         console.log("загаданное число "+ number);
         
+    if (count >= 1){
         if (userNumber === 0 ) {alert(`Игра окончена`)}
         else if (number == userNumber) {
-            alert("Вы угадали!");
+            alert(`Вы угадали!`);
             if (result = confirm("Хотели бы сыграть еще?")){
                 number = Math.floor(Math.random() * 10) + 1;
                 bot();
@@ -15,66 +17,30 @@ function bot() {
             else {alert(`Пока!`); return;}
         }
         else if (number < userNumber) {
-            alert(`Загаданное число меньше`);
+            count--;
+            alert(`Загаданное число меньше, у вас осталось ${count} попыток`);
             bot()
         }
         else if (number > userNumber) {
-            alert(`Загаданное число больше`);
+            count--;
+            alert(`Загаданное число больше, у вас осталось ${count} попыток`);
             bot()
         }
         else if (isNaN(userNumber)) {
-            alert(`Это не число`);
+            count--;
+            alert(`Это не число, у вас осталось ${count} попыток`);
             bot()
         }
         
         }
-    
-    bot();
+        else {
+            let result = confirm(`Попытки закончились, хотите сыграть ещё?`)
+            if (result){
+                count = 3;
+                bot();
+            }
+            else {alert(`Ну как хотите, пока!`);}
+        }
+}
 
-// function bot(b) {
-//     // let number = Math.floor(Math.random() * 10) + 1;
-//     userNumber = Number(prompt("Введите любое число от 1 до 10"));
-//     let number = 5;
-//     console.log("загаданноен число "+number);
-//     // let i = 0; 
-//     let tryP = 10; 
-    
-//     if (number == b) {
-//         alert("Вы угадали!");
-//         if (result = confirm("Вы угадали!!! Хотели бы сыграть еще?")){
-//             bot(b);
-//         }
-//     }
-//     if (number < b &&) {
-//         alert(`Загаданное число меньше, у вас осталось ${tryP-1} попыток`);
-//         // tryP--;
-//         // i++;
-//         console.log(`i равно ${i}, а try равно ${tryP}`);
-//         bot(b)
-//     }
-//     if (number > b) {
-//         alert(`Загаданное число больше, у вас осталось ${tryP-1} попыток`);
-//         // tryP--;
-//         // i++;
-//         console.log(`i равно ${i}, а try равно ${tryP}`);
-//         bot(b)
-//     }
-//     if (isNaN(b)) {
-//         alert(`Это не число, у вас осталось ${tryP-1} попыток`);
-//         // tryP--;
-//         // i++;
-//         console.log(`i равно ${i}, а try равно ${tryP}`);
-//         bot(b)
-//     }
-//     else{
-//         if (result = confirm("Попытки закончились, хотите сыграть еще?")){
-//             bot(b);
-//         }
-//         else {
-//             alert("Ну и ладно, пока!");
-            
-//         }
-//       }
-//     }
-
-// bot(userNumber);
+bot();
