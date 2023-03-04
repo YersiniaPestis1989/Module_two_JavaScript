1fb06cdbfea5cf86172cccb0314ca3ffde8af64f
YersiniaPestis1989 <azentsova@list.ru>
2023-03-04 21:48:22 +0700

    1 ch

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,57 +1,80 @@
 "use sctrict"
-let userNumber = Number(prompt("Введите любое число от 1 до 10"));
 
-// while (isNaN(userNumber) || (userNumber > 10)) {
-//     alert("Введите данные заново, обращая внимание на формат");
+let number = Math.floor(Math.random() * 10) + 1;
+function bot() {
+        userNumber = Number(prompt("Введите любое число от 1 до 10"));
+        console.log("загаданное число "+ number);
+        
+        if (userNumber === 0 ) {alert(`Игра окончена`)}
+        else if (number == userNumber) {
+            alert("Вы угадали!");
+            if (result = confirm("Хотели бы сыграть еще?")){
+                number = Math.floor(Math.random() * 10) + 1;
+                bot();
+            }
+            else {alert(`Пока!`); return;}
+        }
+        else if (number < userNumber) {
+            alert(`Загаданное число меньше`);
+            bot()
+        }
+        else if (number > userNumber) {
+            alert(`Загаданное число больше`);
+            bot()
+        }
+        else if (isNaN(userNumber)) {
+            alert(`Это не число`);
+            bot()
+        }
+        
+        }
+    
+    bot();
+
+// function bot(b) {
+//     // let number = Math.floor(Math.random() * 10) + 1;
 //     userNumber = Number(prompt("Введите любое число от 1 до 10"));
-// }
-// console.log("мое число "+userNumber);
-// console.log("загаданноен число "+number);
-
-function bot(b) {
-    // let number = Math.floor(Math.random() * 10) + 1;
-    userNumber = Number(prompt("Введите любое число от 1 до 10"));
-    let number = 5;
-    console.log("загаданноен число "+number);
-    let i = 0; 
-    let tryP = 10; 
-    for (tryP--, i++) {
-    if (number == b && i < tryP) {
-        alert("Вы угадали!");
-        if (result = confirm("Вы угадали!!! Хотели бы сыграть еще?")){
-            bot(b);
-        }
-    }
-    if (number < b && i < tryP) {
-        alert(`Загаданное число меньше, у вас осталось ${tryP-1} попыток`);
-        // tryP--;
-        // i++;
-        console.log(`i равно ${i}, а try равно ${tryP}`);
-        bot(b)
-    }
-    if (number > b) {
-        alert(`Загаданное число больше, у вас осталось ${tryP-1} попыток`);
-        // tryP--;
-        // i++;
-        console.log(`i равно ${i}, а try равно ${tryP}`);
-        bot(b)
-    }
-    if (isNaN(b)) {
-        alert(`Это не число, у вас осталось ${tryP-1} попыток`);
-        // tryP--;
-        // i++;
-        console.log(`i равно ${i}, а try равно ${tryP}`);
-        bot(b)
-    }
-    else{
-        if (result = confirm("Попытки закончились, хотите сыграть еще?")){
-            bot(b);
-        }
-        else {
-            alert("Ну и ладно, пока!");
+//     let number = 5;
+//     console.log("загаданноен число "+number);
+//     // let i = 0; 
+//     let tryP = 10; 
+    
+//     if (number == b) {
+//         alert("Вы угадали!");
+//         if (result = confirm("Вы угадали!!! Хотели бы сыграть еще?")){
+//             bot(b);
+//         }
+//     }
+//     if (number < b &&) {
+//         alert(`Загаданное число меньше, у вас осталось ${tryP-1} попыток`);
+//         // tryP--;
+//         // i++;
+//         console.log(`i равно ${i}, а try равно ${tryP}`);
+//         bot(b)
+//     }
+//     if (number > b) {
+//         alert(`Загаданное число больше, у вас осталось ${tryP-1} попыток`);
+//         // tryP--;
+//         // i++;
+//         console.log(`i равно ${i}, а try равно ${tryP}`);
+//         bot(b)
+//     }
+//     if (isNaN(b)) {
+//         alert(`Это не число, у вас осталось ${tryP-1} попыток`);
+//         // tryP--;
+//         // i++;
+//         console.log(`i равно ${i}, а try равно ${tryP}`);
+//         bot(b)
+//     }
+//     else{
+//         if (result = confirm("Попытки закончились, хотите сыграть еще?")){
+//             bot(b);
+//         }
+//         else {
+//             alert("Ну и ладно, пока!");
             
-        }
-      }
-    }
-}
-bot(userNumber);
+//         }
+//       }
+//     }
+
+// bot(userNumber);
