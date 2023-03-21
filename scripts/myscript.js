41a9b49ce35eec76b45e7ee83537457c0fea0b98
YersiniaPestis1989 <azentsova@list.ru>
2023-03-21 20:11:12 +0700

    1

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,89 +1,36 @@
 "use sctrict"
-let income = document.querySelector(`#income`)
-let money; // месячный доход
-income.addEventListener(`input`, (e) => {
-    money = Number(e.target.value);
-})
+let users = fetch(`https://reqres.in/api/users?per_page=12`).then((e) => {
+    return e.json()
+}
+);
 
-let additionalIncome = document.querySelector(`#additionalIncome`);
-let profit; //дополнительный доход
-additionalIncome.addEventListener(`input`, (e) => {
-    profit = e.target.value;
-})
+users.then((result) => {
+    console.log(`Фамилии всех пользователей: `)
+    result.data.forEach(element => {
+        console.log(element.last_name)
+    });
+}
+);
 
-let sumAdditionalIncome = document.querySelector(`#sumAdditionalIncome`);
-let extraMoney; // возможный доход
-sumAdditionalIncome.addEventListener(`input`, (e) => {
-    extraMoney = Number(e.target.value);
-})
+users.then((result) => {
+    console.log(`Пользователи, фамилии которых начинаются с F: `)
+    result.data.forEach(element => {
+        if(element.last_name.includes(`F`)){console.log(element)}
+    });
+}
+);
 
-let expensesInput = document.querySelector(`#expenses`);
-let expenses; // возможные расходы
-expensesInput.addEventListener(`input`, (e) => {
-    expenses = e.target.value;
-});
+users.then((result) => {
+    let user = result.data.reduce((arr, curr) => {
+        return arr + (curr.first_name + " " + curr.last_name + ", ")
+    }, "");
+    user = user.substring(0, user.length - 2);
+    console.log(`Наша база содержит данные следующих пользователей ${user}`)
+}
+);
 
-let amountInput = document.querySelector(`#amount`);
-let amount; // обязательные расходы
-amountInput.addEventListener(`input`, (e) => {
-    amount = Number(e.target.value);
-});
-
-let savings = document.querySelector(`#savings`);
-let purpose; //цель
-savings.addEventListener(`input`, (e) => {
-    purpose = Number(e.target.value);
-})
-
-let perionInput = document.querySelector(`#period`);
-let period; // период накопления
-perionInput.addEventListener(`input`, (e) => {
-    period = Number(e.target.value);
-})
-
-const getAccumulatedIncome = (x, y, z) => {return (x + y) - z};
-const getTargetMonth = (sum, purpose) => {return Math.ceil(purpose/sum)};
-
-const btnResult = document.querySelector(".calculator__items-submit");
-btnResult.addEventListener(`click`, () => {
-    let AccumulatedIncome = getAccumulatedIncome(money, extraMoney, amount);
-    let budgetDay = Math.floor(AccumulatedIncome/30);
-    let budgetMounth = Math.ceil(purpose/AccumulatedIncome);
-    
-
-    document.querySelector(`#sumMoney`).textContent = AccumulatedIncome; // прибыль
-    document.querySelector(`#sumMoneyDay`).textContent = budgetDay; // бюджет на день
-    document.querySelector(`#sumMoneyPurpose`).textContent = purpose;
-    document.querySelector(`#purpose`).textContent = getTargetMonth(AccumulatedIncome, purpose);
-    if (Math.floor(budgetDay) > 6000)
-        {
-            document.querySelector(`#incomeLevel`).textContent = "У вас высокий уровень дохода";
-        }
-        else if  (budgetDay > 3000 && budgetDay < 6000)
-        {
-            document.querySelector(`#incomeLevel`).textContent = "У вас средний уровень дохода";
-        }
-        else if  (budgetDay > 0 && budgetDay < 3000)
-        {
-            document.querySelector(`#incomeLevel`).textContent = "К сожалению, у вас уровень дохода ниже среднего";
-        }
-        else if (budgetDay < 0) {
-            document.querySelector(`#incomeLevel`).textContent = "Что-то пошло не так";
-        }
-})
-
-// function proof(item, msg) {
-//     while (isNaN(item) || (item ==="") || item === 0){ item = Number(prompt(msg))}
-// }
-// proof(money, `Ваш месячный доход`);
-// proof(amount, `Во сколько обойдутся обязательные статьи расходов?`);
-// proof(extraMoney, `Перечислите возможный доход за ваши дополнительные работы: ${profit}?`);
-
-
-// if(getTargetMonth(accumulatedIncome, purpose) < 0){
-//     console.log(`Цель не будет достигнута`);
-// }
-// else {
-//     console.log(`Ваша цель накопить ${purpose} рублей с учетом всех ваших расходов будет достигнута через`, getTargetMonth(accumulatedIncome, purpose) + ' месяца');
-// }
-
+users.then((result) => {
+    console.log(`Все ключи: `)
+    console.log(Object.keys(result.data[0]))
+}
+);
